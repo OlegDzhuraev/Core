d15b54bf5f8a3a0af734ee24aa2cafd4bd20dd27
Oleg Dzhuraev <godlikeaurora@gmail.com>
2025-03-23 14:27:54 +0300

    [+] New Color extensions support.
    [*] Old Color extensions were reworked.

--- InsaneOne/Core/Sources/Extensions/ColorExtensions.cs
+++ InsaneOne/Core/Sources/Extensions/ColorExtensions.cs
@@ -4,28 +4,38 @@ namespace InsaneOne.Core
 {
     public static class ColorExtensions
     {
-        public static Color32 SetAlpha(this Color32 color, byte value)
-        {
-            color = new Color32(color.r, color.g, color.b, value);
-			
-            return color;
-        }
+        public static Color32 GetWithR(this Color32 c, byte value) => new(value, c.g, c.b, c.a);
+        public static Color GetWithR(this Color c, float value) => new(value, c.g, c.b, c.a);
 
-        public static Color SetAlpha(this Color color, float value)
-        {
-            color = new Color(color.r, color.g, color.b, value);
-			
-            return color;
-        }
+        public static Color32 GetWithG(this Color32 c, byte value) => new(c.r, value, c.b, c.a);
+        public static Color GetWithG(this Color c, float value) => new(c.r, value, c.b, c.a);
 
-        public static string GetHexValue(this Color32 inputColor, bool isNeededAlpha = false)
+        public static Color32 GetWithB(this Color32 c, byte value) => new(c.r, c.g, value, c.a);
+        public static Color GetWithB(this Color c, float value) => new(c.r, c.g, value, c.a);
+
+        public static Color32 GetWithAlpha(this Color32 c, byte value) => new(c.r, c.g, c.b, value);
+        public static Color GetWithAlpha(this Color color, float value) => new(color.r, color.g, color.b, value);
+
+        public static void SetR(this Color32 c, out Color32 target, byte value) => target = new Color32(value, c.g, c.b, c.a);
+        public static void SetR(this Color c, out Color target, float value) => target = new Color(value, c.g, c.b, c.a);
+
+        public static void SetG(this Color32 c, out Color32 target, byte value) => target = new Color32(c.r, value, c.b, c.a);
+        public static void SetG(this Color c, out Color target, float value) => target = new Color(c.r, value, c.b, c.a);
+
+        public static void SetB(this Color32 c, out Color32 target, byte value) => target = new Color32(c.r, c.g, value, c.a);
+        public static void SetB(this Color c, out Color target, float value) => target = new Color(c.r, c.g, value, c.a);
+
+        public static void SetAlpha(this Color32 c, out Color32 target, byte value) => target = new Color32(c.r, c.g, c.b, value);
+        public static void SetAlpha(this Color c, out Color target, float value) => target = new Color(c.r, c.g, c.b, value);
+
+        public static string GetHexValue(this Color32 c, bool isNeededAlpha = false)
         {
             var result = "#";
 
-            result += inputColor.r.ToString("X2") + inputColor.g.ToString("X2") + inputColor.b.ToString("X2");
+            result += c.r.ToString("X2") + c.g.ToString("X2") + c.b.ToString("X2");
 
             if (isNeededAlpha)
-                result += inputColor.a.ToString("X2");
+                result += c.a.ToString("X2");
 
             return result;
         }
