a629fb694808f33e9ad07ba34ad6305763d4159d
Oleg Dzhuraev <godlikeaurora@gmail.com>
2025-10-26 15:45:44 +0300

    [*] Minor edit

--- InsaneOne/Core/Sources/Data/CoreData.cs
+++ InsaneOne/Core/Sources/Data/CoreData.cs
@@ -22,42 +22,43 @@ namespace InsaneOne.Core
         [Header("Setup Project Tool settings")]
         public List<LinkHolder> GitPackages = new ()
         {
-            new LinkHolder() { Name = "Modifiers", Link = $"https://github.com/{RepoName}/Modifiers.git" },
-            new LinkHolder() { Name = "Perseids Pooling", Link = $"https://github.com/{RepoName}/PerseidsPooling.git" },
-            new LinkHolder() { Name = "NavMesh Avoidance", Link = $"https://github.com/{RepoName}/NavMeshAvoidance.git" },
-            new LinkHolder() { Name = "Tags", Link = $"https://github.com/{RepoName}/Tags.git" },
-            new LinkHolder() { Name = "Serialize Reference Drawer", Link = "https://github.com/mackysoft/Unity-SerializeReferenceExtensions.git?path=Assets/MackySoft/MackySoft.SerializeReferenceExtensions" },
-            new LinkHolder() { Name = "Toolbar Extender", Link = "https://github.com/marijnz/unity-toolbar-extender.git" },
-            new LinkHolder() { Name = "Type reference", Link = "https://github.com/SolidAlloy/ClassTypeReference-for-Unity.git" },
-            new LinkHolder() { Name = "Scene reference", Link = "https://github.com/starikcetin/Eflatun.SceneReference.git" },
-            new LinkHolder() { Name = "Tri Inspector", Link = "https://github.com/codewriter-packages/Tri-Inspector.git" },
+            new LinkHolder { Name = "Modifiers", Link = $"https://github.com/{RepoName}/Modifiers.git" },
+            new LinkHolder { Name = "Perseids Pooling", Link = $"https://github.com/{RepoName}/PerseidsPooling.git" },
+            new LinkHolder { Name = "NavMesh Avoidance", Link = $"https://github.com/{RepoName}/NavMeshAvoidance.git" },
+            new LinkHolder { Name = "Tags", Link = $"https://github.com/{RepoName}/Tags.git" },
+            new LinkHolder { Name = "Serialize Reference Drawer", Link = "https://github.com/mackysoft/Unity-SerializeReferenceExtensions.git?path=Assets/MackySoft/MackySoft.SerializeReferenceExtensions" },
+            new LinkHolder { Name = "Toolbar Extender", Link = "https://github.com/marijnz/unity-toolbar-extender.git" },
+            new LinkHolder { Name = "Type reference", Link = "https://github.com/SolidAlloy/ClassTypeReference-for-Unity.git" },
+            new LinkHolder { Name = "Scene reference", Link = "https://github.com/starikcetin/Eflatun.SceneReference.git" },
+            new LinkHolder { Name = "Tri Inspector", Link = "https://github.com/codewriter-packages/Tri-Inspector.git" },
+            new LinkHolder { Name = "Bookmark everything", Link = "https://github.com/OlegDzhuraev/bookmark-everything.git" },
         };
         
         public List<LinkHolder> Packages = new ()
         {
-            new LinkHolder() { Name = "Input System", Link = "com.unity.inputsystem" },
-            new LinkHolder() { Name = "Post Effects", Link = "com.unity.postprocessing" },
-            new LinkHolder() { Name = "Recorder", Link = "com.unity.recorder" },
-            new LinkHolder() { Name = "Cinemachine", Link = "com.unity.cinemachine" },
-            new LinkHolder() { Name = "Animation Rigging", Link = "com.unity.animation.rigging" },
-            new LinkHolder() { Name = "Terrain Tools", Link = "com.unity.terrain-tools" },
+            new LinkHolder { Name = "Input System", Link = "com.unity.inputsystem" },
+            new LinkHolder { Name = "Post Effects", Link = "com.unity.postprocessing" },
+            new LinkHolder { Name = "Recorder", Link = "com.unity.recorder" },
+            new LinkHolder { Name = "Cinemachine", Link = "com.unity.cinemachine" },
+            new LinkHolder { Name = "Animation Rigging", Link = "com.unity.animation.rigging" },
+            new LinkHolder { Name = "Terrain Tools", Link = "com.unity.terrain-tools" },
         };
         
         public List<LinkHolder> AssetLinks = new ()
         {
-            new LinkHolder() { Name = "DOTween", Link = "https://assetstore.unity.com/packages/tools/animation/dotween-hotween-v2-27676" },
-            new LinkHolder() { Name = "More Effective Coroutines", Link = "https://assetstore.unity.com/packages/tools/animation/more-effective-coroutines-free-54975" },
-            new LinkHolder() { Name = "HotReload", Link = "https://assetstore.unity.com/packages/tools/utilities/hot-reload-edit-code-without-compiling-254358" },
-            new LinkHolder() { Name = "ReWired", Link = "https://assetstore.unity.com/packages/tools/utilities/rewired-21676" },
-            new LinkHolder() { Name = "Odin Inspector", Link = "https://assetstore.unity.com/packages/tools/utilities/odin-inspector-and-serializer-89041" },
-            new LinkHolder() { Name = "vFolders 2", Link = "https://assetstore.unity.com/packages/tools/utilities/vfolders-2-255470" },
-            new LinkHolder() { Name = "FMOD", Link = "https://www.fmod.com/unity" },
-            new LinkHolder() { Name = "MicroSplat Terrain", Link = "https://assetstore.unity.com/packages/tools/terrain/microsplat-96478" },
-            new LinkHolder() { Name = "NG Soft Shadows", Link = "https://assetstore.unity.com/packages/vfx/shaders/next-gen-soft-shadows-137380" },
-            new LinkHolder() { Name = "Dynamic Decals", Link = "https://github.com/EricFreeman/DynamicDecals" },
-            new LinkHolder() { Name = "Prototype Materials", Link = "https://assetstore.unity.com/packages/2d/textures-materials/gridbox-prototype-materials-129127" },
-            new LinkHolder() { Name = "Lit Particles Materials", Link = "https://assetstore.unity.com/packages/vfx/shaders/particle-shaders-vol-1-35069" },
-            new LinkHolder() { Name = "Amplify Shader Editor", Link = "https://assetstore.unity.com/packages/tools/visual-scripting/amplify-shader-editor-68570" },
+            new LinkHolder { Name = "DOTween", Link = "https://assetstore.unity.com/packages/tools/animation/dotween-hotween-v2-27676" },
+            new LinkHolder { Name = "More Effective Coroutines", Link = "https://assetstore.unity.com/packages/tools/animation/more-effective-coroutines-free-54975" },
+            new LinkHolder { Name = "HotReload", Link = "https://assetstore.unity.com/packages/tools/utilities/hot-reload-edit-code-without-compiling-254358" },
+            new LinkHolder { Name = "ReWired", Link = "https://assetstore.unity.com/packages/tools/utilities/rewired-21676" },
+            new LinkHolder { Name = "Odin Inspector", Link = "https://assetstore.unity.com/packages/tools/utilities/odin-inspector-and-serializer-89041" },
+            new LinkHolder { Name = "vFolders 2", Link = "https://assetstore.unity.com/packages/tools/utilities/vfolders-2-255470" },
+            new LinkHolder { Name = "FMOD", Link = "https://www.fmod.com/unity" },
+            new LinkHolder { Name = "MicroSplat Terrain", Link = "https://assetstore.unity.com/packages/tools/terrain/microsplat-96478" },
+            new LinkHolder { Name = "NG Soft Shadows", Link = "https://assetstore.unity.com/packages/vfx/shaders/next-gen-soft-shadows-137380" },
+            new LinkHolder { Name = "Dynamic Decals", Link = "https://github.com/EricFreeman/DynamicDecals" },
+            new LinkHolder { Name = "Prototype Materials", Link = "https://assetstore.unity.com/packages/2d/textures-materials/gridbox-prototype-materials-129127" },
+            new LinkHolder { Name = "Lit Particles Materials", Link = "https://assetstore.unity.com/packages/vfx/shaders/particle-shaders-vol-1-35069" },
+            new LinkHolder { Name = "Amplify Shader Editor", Link = "https://assetstore.unity.com/packages/tools/visual-scripting/amplify-shader-editor-68570" },
         };
         
         [Header("Debugging")]
