93a6ebc57f30c9f49a52e12fe74bc8d0315c7bc8
Oleg Dzhuraev <godlikeaurora@gmail.com>
2025-11-29 15:47:20 +0300

    [*] Some localization changes

--- InsaneOne/Core/Sources/Localization/UI/LocalizedTMPText.cs
+++ InsaneOne/Core/Sources/Localization/UI/LocalizedTMPText.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 
 namespace InsaneOne.Core.Locales
 {
+    [DisallowMultipleComponent]
     public sealed class LocalizedTMPText : Element<LocalizedTextViewModel>, ILocalized
     {
         [SerializeField, Localized] string localizationId;
+        [SerializeField] bool addUiTextIfNotFound = true;
+        [SerializeField] bool initializeOnStart = true;
 
         TMP_Text text;
 
-        void Awake() => text = GetComponent<TMP_Text>();
+        void Awake()
+        {
+            text = GetComponent<TMP_Text>();
+
+            if (addUiTextIfNotFound && !text)
+                text = gameObject.AddComponent<TextMeshProUGUI>();
+        }
 
         void Start()
         {
-            if (ViewModel != null && ViewModel.AutoReload)
+            if (ViewModel is { AutoReload: true } || initializeOnStart && ViewModel == null)
                 ReloadLocalization();
         }
 
         public void ReloadLocalization()
         {
+            if (string.IsNullOrWhiteSpace(localizationId) && gameObject.activeSelf)
+            {
+                CoreUnityLogger.I.Log($"Empty localization id on text object [{name}]!", LogLevel.Warning);
+                return;
+            }
+
             if (ViewModel == null)
                 SetViewModel(new LocalizedTextViewModel(localizationId));
 
